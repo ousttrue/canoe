e82ece37103369723bc771e8423003e29fd74c0d
ousttrue <ousttrue@gmail.com>
2022-03-07 00:15:59 +0900

    BeautifulSoupLexer

--- src/canoe/client.py
+++ src/canoe/client.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, List, Tuple
 import aiohttp
 import bs4
 import prompt_toolkit.layout.containers
@@ -11,26 +11,85 @@ import prompt_toolkit.formatted_text
 import prompt_toolkit.widgets
 
 
+class BeautifulSoupLexer(prompt_toolkit.lexers.Lexer):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.lines: List[prompt_toolkit.formatted_text.StyleAndTextTuples] = [
+            [('', '')]]
+        self.title = ''
+
+    def lex_document(self, document: prompt_toolkit.document.Document) -> Callable[[int], prompt_toolkit.formatted_text.StyleAndTextTuples]:
+        return self.get_line
+
+    def get_line(self, index: int) -> prompt_toolkit.formatted_text.StyleAndTextTuples:
+        return self.lines[index]
+
+    def push(self, text: str, style: str):
+        self.lines[-1].append((style, text))
+
+    def new_line(self):
+        self.lines.append([])
+
+    def traverse(self, e: bs4.PageElement, style: str):
+        match e:
+            case bs4.Tag() as tag:
+                match tag.name:
+                    case 'title':
+                        self.title = tag.text
+                    case 'a':
+                        style = '#0044ff underline'
+                    case 'form':
+                        self.push('form', '#44ff00 underline')
+                    case 'input':
+                        self.push('input', '#44ff00 underline')
+                    case 'p' | 'div':
+                        self.new_line()
+                for child in tag.children:
+                    self.traverse(child, style)
+            case bs4.element.NavigableString():
+                self.push(e.text, style)
+            case bs4.element.Doctype():
+                pass
+            case _:
+                t = type(e)
+                raise RuntimeError(f'unknwon: {t}')
+
+    def tex_html(self, html: str) -> Tuple[str, str]:
+        soup = bs4.BeautifulSoup(html, 'html.parser')
+        self.lines = [[]]
+        self.title = ''
+        for e in soup.children:
+            self.traverse(e, '')
+
+        def merge_line(line):
+            return ''.join(text for style, text in line)
+        lines = [merge_line(line) for line in self.lines]
+        return ('\n'.join(lines), self.title)
+
+
 class Client:
     def __init__(self) -> None:
+        self.lexer = BeautifulSoupLexer()
         self.title = ''
         self.address = ''
         self.status = ''
         self._wrap_lines = False
-        self.text = ''
 
-        self.buffer = prompt_toolkit.buffer.Buffer()
+        self.read_only = True
+        self.buffer = prompt_toolkit.buffer.Buffer(
+            read_only=prompt_toolkit.filters.Condition(lambda: self.read_only))
 
         self.control = prompt_toolkit.layout.controls.BufferControl(
             buffer=self.buffer,
-            # lexer=BeautifulSoupLexer(),
+            lexer=self.lexer,
             # input_processors=input_processors,
             include_default_input_processors=False,
             # preview_search=True,
             # search_buffer_control=search_buffer_control
         )
 
-        @prompt_toolkit.filters.Condition
+        @ prompt_toolkit.filters.Condition
         def wrap_lines() -> bool:
             return self._wrap_lines
 
@@ -56,28 +115,8 @@ class Client:
             async with session.get(url) as response:
                 self.status = f"{response.status}: {response.headers['content-type']}"
                 html = await response.text()
-                self.on_get(html)
-
-    def on_get(self, html: str):
-        # text = []
-        soup = bs4.BeautifulSoup(html, 'html.parser')
-        for e in soup.descendants:
-            match e:
-                case bs4.Tag() as tag:
-                    pass
-                case bs4.element.Doctype():
-                    pass
-                case bs4.element.NavigableString():
-                    # text.append(('', e.text))
-                    pass
-                case _:
-                    t = type(e)
-                    raise RuntimeError(f'unknwon: {t}')
-            if soup.title:
-                self.title = soup.title.get_text()
-            self.buffer.text = soup.get_text()
-
-        # self.text = text
-
-    def get_text(self):
-        return self.text
+        text, title = self.lexer.tex_html(html)
+        self.title = title
+        self.read_only = False
+        self.buffer.text = text
+        self.read_only = True

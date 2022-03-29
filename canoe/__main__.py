ec73449b59faa83533285d072ed4e5f2e0bf1ca5
ousttrue <ousttrue@gmail.com>
2022-03-29 20:04:01 +0900

    run

--- canoe/__main__.py
+++ canoe/__main__.py
@@ -75,4 +75,4 @@ async def main():
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     logger.debug('start')
-    asyncio.get_event_loop().run_until_complete(main())
+    asyncio.run(main())
